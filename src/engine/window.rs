f36c900b92241a77e4df5e2ee12aaada119b39ea
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-09-22 19:04:34 +0200

    Move window logic to engine/window.rs

---
+++ src/engine/window.rs
@@ -0,0 +1,28 @@
+extern crate glfw;
+
+use glfw::{Action, Context, Key};
+
+pub fn run() {
+	let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).expect("Failed to initialize GLFW");
+
+	let (mut window, events) = glfw
+		.create_window(300, 300, "Oxidize", glfw::WindowMode::Windowed)
+		.expect("Failed to create GLFW window.");
+
+	window.set_key_polling(true);
+	window.make_current();
+
+	while !window.should_close() {
+		glfw.poll_events();
+		for (_, event) in glfw::flush_messages(&events) {
+			handle_window_event(&mut window, event);
+		}
+	}
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+	match event {
+		glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
+		_ => {}
+	}
+}
